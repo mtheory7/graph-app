6ff1627c0878b0faa273161546cd609b7a3e69ba
Michael Stuhlman <mstuhlman@configureone.com>
2020-01-24 10:29:20 -0600

    Test github pages

--- src/App.js
+++ src/App.js
@@ -38,20 +38,19 @@ function App() {
       <div className="App">
         <header className="App-header">
           <div>
-            <h3>Mass</h3>
-            <PieChart label="Mass"
-                      data={[["Gold", GRAMS_OF_GOLD], ["Silver", GRAMS_OF_SILVER],
-                        ["Platinum", GRAMS_OF_PLATINUM]]}
-                      colors={['#D4AF37', '#e5e4e2', '#C0C0C0']}/>
+            <h3>Mass (grams)</h3>
+            <PieChart
+                data={[["Gold", GRAMS_OF_GOLD], ["Silver", GRAMS_OF_SILVER],
+                  ["Platinum", GRAMS_OF_PLATINUM]]}
+                colors={['#D4AF37', '#e5e4e2', '#C0C0C0']}/>
           </div>
           <div>
-                <h3>USD</h3>
-                <PieChart label="USD Value"
-                          data={[["Gold", calcValueUSD(GRAMS_OF_GOLD, 'Gold')],
-                            ["Silver", calcValueUSD(GRAMS_OF_SILVER, 'Silver')],
-                            ["Platinum",
-                              calcValueUSD(GRAMS_OF_PLATINUM, 'Platinum')]]}
-                          colors={['#D4AF37', '#e5e4e2', '#C0C0C0']}/>
+            <h3>USD Value</h3>
+            <PieChart data={[["Gold", calcValueUSD(GRAMS_OF_GOLD, 'Gold')],
+              ["Silver", calcValueUSD(GRAMS_OF_SILVER, 'Silver')],
+              ["Platinum",
+                calcValueUSD(GRAMS_OF_PLATINUM, 'Platinum')]]}
+                      colors={['#D4AF37', '#e5e4e2', '#C0C0C0']}/>
           </div>
         </header>
       </div>
